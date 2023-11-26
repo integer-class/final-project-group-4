96b7ec2f2b04c2649c2b1ff201e15f3aba93ea10
elianiva <dicha.arkana03@gmail.com>
2023-11-27 02:04:39 +0700

    style: use qualified import

--- app/backend/Presentation/Http/HttpPresenter.php
+++ app/backend/Presentation/Http/HttpPresenter.php
@@ -4,7 +4,6 @@ namespace Presentation\Http;
 
 use Presentation\Http\Attributes\Route;
 use Presentation\Http\Controllers\Controller;
-use ReflectionClass;
 
 class HttpPresenter
 {
@@ -26,7 +25,7 @@ class HttpPresenter
         $request_method = $_SERVER['REQUEST_METHOD'];
 
         foreach ($this->controllers as $controller) {
-            $reflection = new ReflectionClass($controller);
+            $reflection = new \ReflectionClass($controller);
             $methods = $reflection->getMethods();
             foreach ($methods as $method) {
                 $attributes = $method->getAttributes(Route::class);
@@ -35,7 +34,7 @@ class HttpPresenter
                     if ($route->path === $request_path && $route->method === $request_method) {
                         try {
                             $method->invoke($controller);
-                        } catch (ReflectionException $e) {
+                        } catch (\ReflectionException $e) {
                             header("Content-Type: application/json");
                             http_response_code(500);
                             echo json_encode([
