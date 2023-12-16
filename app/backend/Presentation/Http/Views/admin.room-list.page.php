71e26b3a84ddf916d93bba150395eb2a85d91064
elianiva <dicha.arkana03@gmail.com>
2023-12-16 14:30:33 +0700

    fix: room delete was broken

--- app/backend/Presentation/Http/Views/admin.room-list.page.php
+++ app/backend/Presentation/Http/Views/admin.room-list.page.php
@@ -71,7 +71,7 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h1 class="modal-title fs-5" id="deleteRoomModalLabel">Are you sure?</h1>
+                <h1 class="modal-title fs-5" id="deleteRoomModalLabel">Delete this room?</h1>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
@@ -83,7 +83,7 @@
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                 <form id="form" action="/rooms" method="post">
-                    <button type="button" class="btn btn-danger">Delete</button>
+                    <button type="submit" class="btn btn-danger">Delete</button>
                 </form>
             </div>
         </div>
