f7b6ddcfe81c628246c334a8fc44ea9945e89975
elianiva <dicha.arkana03@gmail.com>
2023-12-18 12:53:49 +0700

    feat: add view transition api

--- app/backend/Presentation/Http/Views/Layout/foot.page.php
+++ app/backend/Presentation/Http/Views/Layout/foot.page.php
@@ -1,6 +1,21 @@
 <?php if ($withLayout): ?>
-</div>
-</main>
+    </div>
+    </main>
 <?php endif; ?>
+
+<script>
+    navigation.addEventListener('navigate', function (event) {
+        const destination = event.destination.url;
+        event.intercept({
+            async handler() {
+                const response = await fetch(destination);
+                const documentText = await response.text();
+                document.startViewTransition(() => {
+                    document.documentElement.innerHTML = documentText;
+                })
+            }
+        })
+    })
+</script>
 </body>
 </html>
