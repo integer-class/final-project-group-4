8ff23e032f9b9fc8c773009f5b46c16a7bc85be1
elianiva <dicha.arkana03@gmail.com>
2023-12-20 13:00:42 +0700

    fix: remove viewtransition for now since it breaks several stuff

--- app/backend/Presentation/Http/Views/Layout/foot.page.php
+++ app/backend/Presentation/Http/Views/Layout/foot.page.php
@@ -4,28 +4,28 @@
 <?php endif; ?>
 
 <script>
-    navigation.addEventListener('navigate', function (event) {
-        const destination = event.destination.url;
-        if (event.formData === null) {
-            event.intercept({
-                async handler() {
-                    const response = await fetch(destination);
-                    const documentText = await response.text();
-                    document.startViewTransition(() => {
-                        document.documentElement.innerHTML = documentText;
-                        setTimeout(() => {
-                            window.document.dispatchEvent(new Event("DOMContentLoaded", {
-                                bubbles: true,
-                                cancelable: true
-                            }));
-                            // this seems hacky to rely on timeout but the datatable can't be immediately initialized
-                            // it needs to wait for some time after the dom is ready
-                        }, 50);
-                    })
-                }
-            })
-        }
-    })
+    // navigation.addEventListener('navigate', function (event) {
+    //     const destination = event.destination.url;
+    //     if (event.formData === null) {
+    //         event.intercept({
+    //             async handler() {
+    //                 const response = await fetch(destination);
+    //                 const documentText = await response.text();
+    //                 document.startViewTransition(() => {
+    //                     document.documentElement.innerHTML = documentText;
+    //                     setTimeout(() => {
+    //                         window.document.dispatchEvent(new Event("DOMContentLoaded", {
+    //                             bubbles: true,
+    //                             cancelable: true
+    //                         }));
+    //                         // this seems hacky to rely on timeout but the datatable can't be immediately initialized
+    //                         // it needs to wait for some time after the dom is ready
+    //                     }, 200);
+    //                 })
+    //             }
+    //         })
+    //     }
+    // })
 </script>
 </body>
 </html>
